c9a5af927e811e41836c6d5381ad47b652e9a0e9
tejiri-code <evelynedjere@gmail.com>
2023-06-22 23:27:55 +0000

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -15,35 +15,48 @@ const Portfolio = () => {
     config: { duration: 1000 },
   });
 
-  
-
   return (
     <animated.div className="portfolio" style={fadeIn}>
-      <h1 style={{ color: '#ff6f00' }}>Welcome to My Portfolio</h1>
-      <p>
-        Hello! I'm a creative developer passionate about building awesome web experiences.
-      </p>
-      <animated.div className="skills" style={slideIn}>
-        <h2>Skills</h2>
+      <div className="intro">
+        <img src="your-picture.jpg" alt="Your Picture" />
+        <h1 style={{ color: '#ff6f00' }}>Welcome to My Portfolio</h1>
+        <p>
+          Hello! I'm a creative developer passionate about building awesome web experiences.
+        </p>
+      </div>
+      <div className="socials">
+        <h2>Connect with Me</h2>
         <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Animation</li>
+          <li>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+              GitHub
+            </a>
+          </li>
         </ul>
-      </animated.div>
-      <animated.div className="projects" >
+      </div>
+      <div className="projects">
         <h2>Projects</h2>
         <div className="project">
           <h3>Project 1</h3>
           <p>This is the first project I worked on.</p>
+          <a href="project-1-url" target="_blank" rel="noopener noreferrer">View Project</a>
         </div>
         <div className="project">
           <h3>Project 2</h3>
           <p>This is the second project I worked on.</p>
+          <a href="project-2-url" target="_blank" rel="noopener noreferrer">View Project</a>
         </div>
-      </animated.div>
+      </div>
     </animated.div>
   );
 };
