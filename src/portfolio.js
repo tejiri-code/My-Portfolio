4482d26ad03fc0ee3fef41a1511b34b86b06e9e7
tejiri-code <evelynedjere@gmail.com>
2023-06-22 23:18:41 +0000

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from 'react-dom';
+import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import './styles.css';
+import './index.css';
 
 const Portfolio = () => {
   const fadeIn = useSpring({
@@ -10,19 +9,25 @@ const Portfolio = () => {
     config: { duration: 1000 },
   });
 
+  const slideIn = useSpring({
+    transform: 'translateX(0)',
+    from: { transform: 'translateX(-100%)' },
+    config: { duration: 1000 },
+  });
+
+  const rotate = useSpring({
+    transform: 'rotate(0deg)',
+    from: { transform: 'rotate(-360deg)' },
+    config: { duration: 2000 },
+  });
+
   return (
     <animated.div className="portfolio" style={fadeIn}>
-      <h1>Welcome to My Portfolio</h1>
+      <h1 style={{ color: '#ff6f00' }}>Welcome to My Portfolio</h1>
       <p>
-        Hello! I'm a creative developer passionate about building awesome web
-        experiences.
+        Hello! I'm a creative developer passionate about building awesome web experiences.
       </p>
-      <animated.div
-        className="skills"
-        style={{
-          transform: fadeIn.opacity.interpolate((o) => `scale(${o})`),
-        }}
-      >
+      <animated.div className="skills" style={slideIn}>
         <h2>Skills</h2>
         <ul>
           <li>HTML</li>
@@ -32,12 +37,7 @@ const Portfolio = () => {
           <li>Animation</li>
         </ul>
       </animated.div>
-      <animated.div
-        className="projects"
-        style={{
-          transform: fadeIn.opacity.interpolate((o) => `translateY(${o * 20}px)`),
-        }}
-      >
+      <animated.div className="projects" style={rotate}>
         <h2>Projects</h2>
         <div className="project">
           <h3>Project 1</h3>
