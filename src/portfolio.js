eea526e438bebc2436a1b3ec2d2d428d77fd2c92
tejiri-code <evelynedjere@gmail.com>
2023-06-22 23:20:07 +0000

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
-import './index.css';
+import './styles.css';
 
 const Portfolio = () => {
   const fadeIn = useSpring({
