ae69913fb5c32380e9aca7c5f47a97a98ad84508
tejiri-code <evelynedjere@gmail.com>
2023-06-23 05:31:46 +0000

    chf

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,9 +4,9 @@ import './styles.css';
 import { AiFillLinkedin, AiFillGithub, AiFillMail, AiFillTwitterCircle } from 'react-icons/ai';
 import image from './image.png';
 import connected from './connected.png';
-import agt from './agt.png';
-import linktree from './linktree.png';
 import navigate from './navigate.png';
+import linktree from './linktree.png';
+import agt from './agt.png';
 import quiz from './quiz.png';
 
 function Portfolio() {
