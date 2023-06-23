9fd05c22b8380dd7d98306c63447597c27d12d53
tejiri-code <evelynedjere@gmail.com>
2023-06-23 01:07:49 +0000

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import './styles.css';
 import tej30 from './tej30.jpg';
-
+import {AiFillLinkedin, AiFillGithub, AiFillMail} from 'react-icons/ai';
 const Portfolio = () => {
   const fadeIn = useSpring({
     opacity: 1,
@@ -27,19 +27,25 @@ const Portfolio = () => {
         <h2 className="section-title">Connect with Me</h2>
         <ul className="social-links">
           <li>
-            <a href="https://www.linkedin.com/in/edjere-evelyn-oghenetejiri-5267a9250" target="_blank" rel="noopener noreferrer" class="btn">
+            <a href="https://www.linkedin.com/in/edjere-evelyn-oghenetejiri-5267a9250" target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin size={30}/>
               LinkedIn
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/oghenetejiirii?s=11&t=iqkQNAbtjjHj9gPd2kh0yg" target="_blank" rel="noopener noreferrer" class="btn">
+            <a href="https://twitter.com/oghenetejiirii?s=11&t=iqkQNAbtjjHj9gPd2kh0yg" target="_blank" rel="noopener noreferrer" >
+            <AiFillTwitterCircle/>
               Twitter
             </a>
           </li>
           <li>
-            <a href="https://github.com/tejiri-code" target="_blank" rel="noopener noreferrer"class="btn">
+            <a href="https://github.com/tejiri-code" target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin size={30}/>
               GitHub
             </a>
+          </li>
+          <li>
+          <a className=' hover:text-teal-500'  href="mailto:evelynedjere@gmail.com"> <AiFillMail size={30}/></a>
           </li>
         </ul>
       </div>
