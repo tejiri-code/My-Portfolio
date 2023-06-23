b6592ec778cc6ec884205d39818fa2dc7b216850
tejiri-code <evelynedjere@gmail.com>
2023-06-23 01:43:04 +0000

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,15 +23,10 @@ const Portfolio = () => {
        <div className=' py-10 mb-6 flex justify-between'>
          <h1 className=' text-xl font-burtons'>Tejiri</h1>
          <ul className=' flex items-center'>
-            <li>
                 <BsFillMoonStarsFill className=' cursor-pointer text-xl'/>
-                </li>
-            <li>
                 <a className=' bg-blue-600 text-white px-4 py-2 rounded-md ml-8' href="/westliam resume.png" download> 
-  
                     Resume
-                    </a>
-                    </li>
+                    </a>   
          </ul>
        </div>
       <h1 >Welcome to My Portfolio</h1>
