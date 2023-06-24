f4cbe4f407ac5f8787789dc368d6fcb005778a8b
tejiri-code <evelynedjere@gmail.com>
2023-06-24 20:48:19 +0000

    resume added

--- src/portfolio.js
+++ src/portfolio.js
@@ -8,6 +8,7 @@ import navigate from './navigate.png';
 import linktree from './linktree.png';
 import agt from './agt.png';
 import quiz from './quiz.png';
+import Resume from './Resume.png';
 
 function Portfolio() {
   const fadeIn = useSpring({
@@ -38,6 +39,10 @@ function Portfolio() {
           <h2 className="text-2xl py-2 text-teal-500 font-medium md:text-6xl">
             Edjere Evelyn Oghenetejiri
           </h2>
+           <a className='resume' href="/Resume.png" download> 
+  
+                    Resume
+                    </a>
           <h3 className="text-2xl py-2 md:text-3xl">Frontend Developer</h3>
           <p className="text-base py-5 leading-8 text-gray-800 max-w-1xl mx-auto dark:text-white">
             Hello! I'm a creative developer passionate about building awesome web experiences.
