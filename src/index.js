bc73f0d88ff67f5f6e39dc024fac1b9083e6101c
tejiri-code <evelynedjere@gmail.com>
2023-08-08 03:04:40 +0000

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Portfolio from "./portfolio.js"; // Update the file name with lowercase "p"
 import "./styles.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import './styles.css';
 import { AiFillLinkedin, AiFillGithub, AiFillMail, AiFillTwitterCircle } from 'react-icons/ai';
@@ -11,6 +12,9 @@ import quiz from './quiz.png';
 import investify from './investify.png';
 import spend from './spend.png'
 import recp from './ReCP.png'
+import moda from './moda.png'
+
+
 
 function Portfolio() {
   const fadeIn = useSpring({
@@ -30,17 +34,85 @@ function Portfolio() {
     config: { duration: 1000 },
   });
 
+  const sections = document.querySelectorAll('section[id]');
+  window.addEventListener('scroll', scrollActive);
+
+  function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 50;
+      const sectionId = current.getAttribute('id'); // Declare sectionId here
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+      } else {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
+      }
+    });
+  }
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  /*===== MENU SHOW Y HIDDEN =====*/
+  const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+const toggleMenu = () => {
+  setIsNavOpen(!isNavOpen);
+};
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
   return (
     <animated.div className="portfolio" style={fadeIn}>
-      <div className="navbar">
+      <header class="l-header">
+            <nav class="nav bd-grid">
+                <div>
+                    <a href="#" class="nav__logo">TEJIRI CODES</a>
+                </div>
+
+                <div class={isNavOpen ? 'nav__menu show' : 'nav__menu'} id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                        <li class="nav__item"><a href="#portfolio" class="nav__link">Projects</a></li>
+                        <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+                        <button onClick={handleDownload} class="btn">Resume</button>
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+                    <i class='bx bx-menu'>
+                      
+                    </i>
+                </div>
+            </nav>
+        </header>
+      <div className="navbar" id="home">
       <h1 className="text-xl font-burtons">
   <span className="animated-text">
     Hi! It's Tejiri
   </span>
 </h1>  
-<button onClick={handleDownload} class="btn">Resume</button>
+
       </div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1 id="about">Welcome to My Portfolio</h1>
       <img src={image} alt="Your Picture" className="profile-image" />
       <div className="py-6 mb-12">
         <div className="text-center px-2 py-10">
@@ -48,14 +120,15 @@ function Portfolio() {
             Edjere Evelyn Oghenetejiri
           </h2>
           <h3 className="text-2xl py-2 md:text-3xl">Frontend Developer</h3>
-          <p className="text-base py-5 leading-8 text-gray-800 max-w-1xl mx-auto ">
-            Hello! I'm a creative developer passionate about building awesome web experiences.
+          <p className="text-base py-5 leading-8 text-gray-800 max-w-1xl mx-auto " >
+            Hello! I'm a creative developer passionate about building awesome web experiences.<br/>
            I enjoy collaborating with talented individuals and teams to bring ideas to life and deliver
-    exceptional digital solutions. 
+    exceptional digital solutions. <br/>
      I'm currently open to opportunities. 
           </p>
         </div>
       </div>
+
       <div className="socials">
         <h2 className="section-title">Connect with Me</h2>
         <ul className="social-links">
@@ -90,61 +163,107 @@ function Portfolio() {
         </ul>
       </div>
      
-      <div className="projects">
+     
+      <section class="skills section" id="skills">
       <hr></hr>
-        <h2 className="section-title"><u>Projects</u></h2>
+                <h2 class="section-title">Skills</h2>
+
+                <div class="skills__container bd-grid">
+                    <div class="skills__box">
+                        <h3 class="skills__subtitle">Development</h3>
+                        <span class="skills__name">Html</span>
+                        <span class="skills__name">Css</span>
+                        <span class="skills__name">Javascript</span>
+                        <span class="skills__name">Tailwind CSS</span>
+                        <span class="skills__name">Java</span>
+                        <span class="skills__name">React</span>
+                    </div>
+                </div>
+            </section>
+
+            <hr></hr>
+            <h2 className="section-title" id="portfolio"><u>Projects</u></h2>
+      <div className="projects">
+
+        <div className="project">
+          <p className="project-description">Moda Matrix</p>
+          <a className="project-link" href="https://modamatrix.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <img src={moda} alt="ReCP" className="connect" />
+            <button className='button'> Open
+</button>
+          </a>
        
+        </div>
+
         <div className="project">
           <p className="project-description">ReCP</p>
-          <a className="project-link" href="https://spend-w-me.vercel.app/" target="_blank" rel="noopener noreferrer">
+          <a className="project-link" href="https://recep-tejiri-code.vercel.app/" target="_blank" rel="noopener noreferrer">
             <img src={recp} alt="ReCP" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
 
         <div className="project">
           <p className="project-description">Spend W Me</p>
-          <a className="project-link" href="https://recep-tejiri-code.vercel.app/" target="_blank" rel="noopener noreferrer">
+          <a className="project-link" href="https://spend-w-me.vercel.app/" target="_blank" rel="noopener noreferrer">
             <img src={spend} alt="Spend W Me" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">Investify</p>
           <a className="project-link" href="https://investify-me.vercel.app/" target="_blank" rel="noopener noreferrer">
             <img src={investify} alt="Investify" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">ConnectED</p>
           <a className="project-link" href="http://www.connected.net.ng/" target="_blank" rel="noopener noreferrer">
             <img src={connected} alt="ConnectED" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">Navigate</p>
           <a className="project-link" href="https://tejiri-code.github.io/University-Navigation-Map/" target="_blank" rel="noopener noreferrer">
             <img src={navigate} alt="Navigate" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">Linktree Clone</p>
           <a className="project-link" href="https://tejiri-code.github.io/LinkTree-Clone/" target="_blank" rel="noopener noreferrer">
             <img src={linktree} alt="Linktree" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">Automated Timetable Generator</p>
           <a className="project-link" href="https://tejiri-code.github.io/Automated-Timetable-Generator/" target="_blank" rel="noopener noreferrer">
             <img src={agt} alt="AGT" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
         <div className="project">
           <p className="project-description">Quiz Me</p>
           <a className="project-link" href="https://tejiri-code.github.io/Quiz-App/" target="_blank" rel="noopener noreferrer">
             <img src={quiz} alt="Quiz" className="connect" />
+            <button className='button'> Open
+</button>
           </a>
         </div>
       </div>
+     
     </animated.div>
+    
   );
 }
 
